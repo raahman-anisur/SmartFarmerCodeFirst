4e758dc4b05482d94c70301eb6c2c4028fe31606
kafi-anan-rafi <kafianan01@gmail.com>
2022-12-24 15:00:12 +0600

    Migration of Farmer,Loan & Equipment added

---
+++ DAL/Migrations/202212240852113_FarmerLoanAndEquipmentTableAdded.cs
@@ -0,0 +1,54 @@
+﻿namespace DAL.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class FarmerLoanAndEquipmentTableAdded : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Equipments",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        SellerID = c.Int(nullable: false),
+                        Price = c.Int(nullable: false),
+                        Address = c.String(nullable: false),
+                        Quantity = c.Int(nullable: false),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+            CreateTable(
+                "dbo.Farmers",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Name = c.String(nullable: false),
+                        Address = c.String(nullable: false),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+            CreateTable(
+                "dbo.Loans",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Amount = c.Int(nullable: false),
+                        FarmerId = c.Int(nullable: false),
+                        BorrowDate = c.String(nullable: false),
+                        TodDate = c.String(nullable: false),
+                        Status = c.String(nullable: false),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+        }
+        
+        public override void Down()
+        {
+            DropTable("dbo.Loans");
+            DropTable("dbo.Farmers");
+            DropTable("dbo.Equipments");
+        }
+    }
+}
